8cb06234f948d2318563221c17c4b565fada1444
Kevin Tran <traankevin@gmail.com>
2025-05-02 17:07:39 -0500

    Added event listeners for playerSelection buttons

--- script.js
+++ script.js
@@ -11,18 +11,9 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    let player = prompt("Rock, Paper, Scissors?");
-    let player2 = player.toLowerCase();
-    return player2;
-}
-
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice =='rock' && computerChoice == 'paper') {
-        console.log("You lose! Paper beats Rock.");
-        computerScore++;
-    }
-    else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+
+    if (humanChoice == 'rock' && computerChoice == 'scissors') {
         console.log("You win! Rock beats Scissors.");
         humanScore++;
     }
@@ -30,20 +21,16 @@ function playRound(humanChoice, computerChoice) {
         console.log("You win! Paper beats Rock.");
         humanScore++;
     }
-    else if (humanChoice == 'paper' && computerChoice == 'scissors') {
-        console.log("You lose! Scissors beat Paper.");
-        computerScore++;
-    }
-    else if (humanChoice == 'scissors' && computerChoice == 'rock') {
-        console.log("You lose! Rock beats Scissors.");
-        computerScore++;
-    }
     else if (humanChoice == 'scissors' && computerChoice == 'paper') {
         console.log("You win! Scissors beat Paper.");
         humanScore++;
     }
     else if (humanChoice == computerChoice) {
         console.log("It's a tie!");
+    }
+    else {
+        console.log("You Lose! " + computerChoice + " beats " + humanChoice);
+        computerScore++;
     }
 }
 
@@ -53,20 +40,24 @@ function playGame() {
     playRound(userSelection, computerSelection);
 }
 
+const rock_button = document.querySelector("#rock_player");
+rock_button.addEventListener("click", function (e) {
+    humanChoice = 'rock';
+    console.log(humanChoice);
+});
+const paper_button = document.querySelector("#paper_player");
+paper_button.addEventListener("click", function (e) {
+    humanChoice = 'paper';
+    console.log(humanChoice);
+});
+const scissors_button = document.querySelector("#scissors_player");
+scissors_button.addEventListener("click", function (e) {
+    humanChoice = 'scissors';
+    console.log(humanChoice);
+});
+
+
+
 let humanScore = 0;
 let computerScore = 0;
-
-for (let i = 0; i < 5; i++) {
-    playGame();
-}
-if (humanScore > computerScore) {
-    console.log("You win the game with a score of: " + humanScore + " to " + computerScore);
-}
-else if (computerScore > humanScore) {
-    console.log("You lose the game with a score of: " + computerScore + " to " + humanScore);
-}
-else {
-    console.log("Its a tie with a score of: " + humanScore + " to " + computerScore);
-}
-
 
